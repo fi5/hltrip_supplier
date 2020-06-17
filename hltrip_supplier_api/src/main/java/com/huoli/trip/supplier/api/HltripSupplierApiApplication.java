f587b0d994769df7dc74391288a9a30edd4167fa
顾刘川 <glcworld@163.com>
2020-06-17 15:14:44 +0800

    Initial commit

---
+++ hltrip_supplier_api/src/main/java/com/huoli/trip/supplier/api/HltripSupplierApiApplication.java
@@ -0,0 +1,13 @@
+package com.huoli.trip.supplier.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HltripSupplierApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HltripSupplierApiApplication.class, args);
+    }
+
+}
