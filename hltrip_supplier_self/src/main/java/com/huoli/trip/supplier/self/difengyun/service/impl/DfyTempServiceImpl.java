da7315232447e4f6cc88c3a655e25b63e1e1dce5
fengzhiqiang <fengzq@133.cn>
2020-06-19 14:56:39 +0800

    1.基础结构

---
+++ hltrip_supplier_self/src/main/java/com/huoli/trip/supplier/self/difengyun/service/impl/DfyTempServiceImpl.java
@@ -0,0 +1,14 @@
+package com.huoli.trip.supplier.self.difengyun.service.impl;
+
+import com.huoli.trip.supplier.self.difengyun.service.DfyTempService;
+
+/**
+ * 描述：<br/>
+ * 版权：Copyright (c) 2011-2020<br>
+ * 公司：活力天汇<br>
+ * 作者：冯志强<br>
+ * 版本：1.0<br>
+ * 创建日期：2020/6/19<br>
+ */
+public class DfyTempServiceImpl implements DfyTempService {
+}
