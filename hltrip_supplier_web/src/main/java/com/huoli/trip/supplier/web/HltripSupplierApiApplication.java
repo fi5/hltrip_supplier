f594557785ce0192a6323e160641bb449f02f164
fengzhiqiang <fengzq@133.cn>
2020-06-22 16:33:59 +0800

    1.日志

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/HltripSupplierApiApplication.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/HltripSupplierApiApplication.java
@@ -11,11 +11,13 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class,
         MongoAutoConfiguration.class, MongoDataAutoConfiguration.class,
         DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+//@ImportResource({"classpath:disconf-config.xml"})
 @ComponentScan({"com.huoli.trip"})
 @EnableFeignClients(basePackages = "com.huoli.trip.supplier.feign.clinet")
 @EnableHystrix
