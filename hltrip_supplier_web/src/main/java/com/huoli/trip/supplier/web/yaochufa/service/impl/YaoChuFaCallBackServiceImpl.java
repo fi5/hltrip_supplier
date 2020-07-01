57a9e9782f31be2cd211baaaf1328a00144e371f
zhouwenbin <zhouwb@133.cn>
2020-07-01 11:49:49 +0800

    退款

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/yaochufa/service/impl/YaoChuFaCallBackServiceImpl.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/yaochufa/service/impl/YaoChuFaCallBackServiceImpl.java
@@ -1,10 +1,15 @@
 package com.huoli.trip.supplier.web.yaochufa.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.huoli.trip.common.constant.ConfigConstants;
+import com.huoli.trip.common.util.ConfigGetter;
+import com.huoli.trip.common.util.HttpUtil;
 import com.huoli.trip.supplier.self.yaochufa.vo.YcfRefundNoticeRequest;
 import com.huoli.trip.supplier.self.yaochufa.vo.basevo.YcfBaseResult;
 import com.huoli.trip.supplier.self.yaochufa.vo.push.OrderStatusInfo;
 import com.huoli.trip.supplier.self.yaochufa.vo.push.YcfPushOrderStatusReq;
 import com.huoli.trip.supplier.web.yaochufa.service.IYaoChuFaCallBackService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +17,20 @@ import org.springframework.stereotype.Service;
  * yaochufa 回调服务
  */
 @Service
+@Slf4j
 public class YaoChuFaCallBackServiceImpl implements IYaoChuFaCallBackService {
 
    public void refundNotice(YcfRefundNoticeRequest request){
-        //发http请求至中台,中台写kafka消息
-    }
+       String url= ConfigGetter.getByFileItemString(ConfigConstants.CONFIG_FILE_NAME_COMMON,"hltrip.centtral")+"/recSupplier/refundNotice";
+       try {
+
+           String res = HttpUtil.doPostWithTimeout(url, JSONObject.toJSONString(request), 10000, null);
+           log.info("中台refundNotice返回:"+res);
+       } catch (Exception e) {
+           log.info("",e);
+       }
+
+   }
 
 
     @Override
