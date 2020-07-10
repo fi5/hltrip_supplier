0d5de7ba1539051e3187cab04a2ae18b82dbd5d3
fengzhiqiang <fengzq@133.cn>
2020-07-10 10:50:42 +0800

    1.mongo配置

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/HltripSupplierWebApplication.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/HltripSupplierWebApplication.java
@@ -3,10 +3,12 @@ package com.huoli.trip.supplier.web;
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,7 +16,8 @@ import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class,
-        DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+        DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class,
+        MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @ImportResource({"classpath:disconf-config.xml"})
 @ComponentScan({"com.huoli.trip"})
 @DubboComponentScan(value = "com.huoli.trip.supplier.web")
