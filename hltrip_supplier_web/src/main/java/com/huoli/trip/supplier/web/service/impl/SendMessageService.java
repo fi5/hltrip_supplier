0b4c3b67be8c0dadce88f0bcd2ba0ed92a785562
fengyanrong <531581747@qq.com>
2020-12-02 15:20:24 +0800

    预览业务处理

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/service/impl/SendMessageService.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/service/impl/SendMessageService.java
@@ -4,7 +4,7 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.huoli.flight.server.api.SmsService;
 import com.huoli.flight.server.api.vo.SmsReq;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.velocity.app.event.implement.EscapeXmlReference;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,11 +17,13 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class SendMessageService {
-    @Reference(group = "flight-dubbo-qa", timeout = 30000, /*check = false,*/ retries = 3)
+
+    @Reference(group = "${flight_dubbo_group}", timeout = 30000, /*check = false,*/ retries = 3)
     private SmsService smsService;
 
     private static final String content1 = "您好，您有新的旅游产品支付订单，订单号：%S；请尽快前往后台管理系统处理，谢谢";
     private static final String content2 = "您好，您有旅游产品退款申请订单，订单号：%S；请尽快前往后台管理系统处理，谢谢。";
+
 
     public void sendMSG(String phone,String orderId,int type){
         SmsReq smsReq = new SmsReq();
