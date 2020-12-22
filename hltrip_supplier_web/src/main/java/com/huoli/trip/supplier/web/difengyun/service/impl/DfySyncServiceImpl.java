9298fc4527275cba51545cda00c670dccf62f98c
fengzhiqiang <fengzq@133.cn>
2020-12-22 15:29:32 +0800

    1.配置

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/difengyun/service/impl/DfySyncServiceImpl.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/difengyun/service/impl/DfySyncServiceImpl.java
@@ -23,7 +23,6 @@ import com.huoli.trip.supplier.web.difengyun.service.DfySyncService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
