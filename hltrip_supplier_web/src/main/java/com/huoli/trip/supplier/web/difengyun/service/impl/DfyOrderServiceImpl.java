0ec3174ece6f82bfde2dac86fd0bbd9d615a5751
zhouwenbin <zhouwb@133.cn>
2021-01-18 16:19:57 +0800

    dbRefundNotify

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/difengyun/service/impl/DfyOrderServiceImpl.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/difengyun/service/impl/DfyOrderServiceImpl.java
@@ -102,7 +102,7 @@ public class DfyOrderServiceImpl implements DfyOrderService {
                         }
                     }
 
-                    log.info("详情这里的payed:"+payed);
+                    log.info("已取消订单详情这里的payed:"+payed);
 
                     if(payed){
                         detail.setOrderStatus("申请退款中");
@@ -111,6 +111,14 @@ public class DfyOrderServiceImpl implements DfyOrderService {
                         if(dbRefundNotify!=null){
                             if(dbRefundNotify.getStatus()==1){
                                 detail.setOrderStatus("已退款");
+                            }else{//这里去实时查一下账单
+                                try {
+                                    processNotify(dbRefundNotify);
+                                    Thread.sleep(100);
+                                }  catch (Exception e) {
+                                    log.info("裡处理退款通知失败了，id={}", dbRefundNotify.getId(), e);
+                                }
+
                             }
 
                         }else{
