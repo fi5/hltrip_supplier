4c4f0578b9cf985e4bbcee8beed347b0f0a173b9
fengzhiqiang <fengzq@133.cn>
2021-03-12 18:03:07 +0800

    1.优化要出发价格日历同步 2.供应商渠道不自动上线

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/service/impl/ProductServiceImpl.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/service/impl/ProductServiceImpl.java
@@ -1,6 +1,8 @@
 package com.huoli.trip.supplier.web.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.google.common.collect.Lists;
+import com.huoli.trip.supplier.api.DynamicProductItemService;
 import com.huoli.trip.supplier.api.ProductService;
 import com.huoli.trip.supplier.web.dao.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +21,19 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductDao productDao;
 
+    @Autowired
+    private DynamicProductItemService dynamicProductItemService;
+
     @Override
     public void updateStatusByCode(String code, int status){
         productDao.updateStatusByCode(code, status);
+        dynamicProductItemService.refreshItemByProductCode(Lists.newArrayList(code));
+
     }
 
     @Override
     public void updateSupplierStatusByCode(String code, int supplierStatus){
         productDao.updateSupplierStatusByCode(code, supplierStatus);
+        dynamicProductItemService.refreshItemByProductCode(Lists.newArrayList(code));
     }
 }
