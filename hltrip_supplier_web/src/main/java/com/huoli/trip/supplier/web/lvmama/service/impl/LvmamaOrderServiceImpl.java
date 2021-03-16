22dbed4d8e1a17f693a1fe098086d516a584fa74
fengyanrong <531581747@qq.com>
2021-03-16 11:31:33 +0800

    feat:驴妈妈订单相关接口初步开发、对象定义

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/lvmama/service/impl/LvmamaOrderServiceImpl.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/lvmama/service/impl/LvmamaOrderServiceImpl.java
@@ -1,9 +1,12 @@
 package com.huoli.trip.supplier.web.lvmama.service.impl;
 
 import com.huoli.trip.supplier.api.LvmamaOrderService;
+import com.huoli.trip.supplier.feign.client.lvmama.client.ILvmamaClient;
+import com.huoli.trip.supplier.feign.client.yaochufa.client.IYaoChuFaClient;
 import com.huoli.trip.supplier.self.lvmama.vo.request.*;
 import com.huoli.trip.supplier.self.lvmama.vo.response.BaseResponse;
 import com.huoli.trip.supplier.self.lvmama.vo.response.OrderResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author lunatic
@@ -13,28 +16,30 @@ import com.huoli.trip.supplier.self.lvmama.vo.response.OrderResponse;
  * @date 2021/3/1517:48
  */
 public class LvmamaOrderServiceImpl implements LvmamaOrderService {
+    @Autowired
+    private ILvmamaClient iLvmamaClient;
     @Override
     public BaseResponse getCheckInfos(ValidateOrderRequest request) {
-        return null;
+        return iLvmamaClient.getCheckInfos(request);
     }
 
     @Override
     public OrderResponse payOrder(OrderPaymentRequest request) {
-        return null;
+        return iLvmamaClient.payOrder(request);
     }
 
     @Override
     public OrderResponse createOrder(CreateOrderRequest request) {
-        return null;
+        return iLvmamaClient.createOrder(request);
     }
 
     @Override
     public OrderResponse cancelOrder(OrderUnpaidCancelRequest request) {
-        return null;
+        return iLvmamaClient.cancelOrder(request);
     }
 
     @Override
     public BaseResponse rufundTicket(OrderCancelRequest request) {
-        return null;
+        return iLvmamaClient.rufundTicket(request);
     }
 }
