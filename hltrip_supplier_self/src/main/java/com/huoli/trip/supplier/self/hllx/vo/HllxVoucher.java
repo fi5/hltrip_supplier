46c04aead145cdb47ff38544c4ceb11fb6c03853
fengzhiqiang <fengzq@133.cn>
2021-04-15 11:18:07 +0800

    1.出团通知

--- hltrip_supplier_self/src/main/java/com/huoli/trip/supplier/self/hllx/vo/HllxVoucher.java
+++ hltrip_supplier_self/src/main/java/com/huoli/trip/supplier/self/hllx/vo/HllxVoucher.java
@@ -10,11 +10,18 @@ public class HllxVoucher implements Serializable {
      * 凭证信息
      */
     private String voucherInfo;
+
     /**
-     * 凭证类型
-     * 1.纯文本  2.二维码 3.PDF 4.手机尾号
+     * 入园方式
+     * 0凭兑换凭证直接入园
+     * 1凭兑换凭证换票入园
      */
-    private int type;
+    private Integer inType;
+
+    /**
+     *   附件类型 1.纯文本  2.二维码 3.PDF 4.手机号 5.身份证 6.二维码或身份证 7.数字码 8.其他证件
+     */
+    private Integer type;
 
     /**
      * 创建时间
