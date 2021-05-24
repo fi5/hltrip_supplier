084fd42b668d234c356d77e87a3d1f2962a296d6
fengyanrong <531581747@qq.com>
2021-05-24 17:10:58 +0800

    feat:lvmama预订前校验业务处理

--- hltrip_supplier_self/src/main/java/com/huoli/trip/supplier/self/lvmama/vo/Travellers.java
+++ hltrip_supplier_self/src/main/java/com/huoli/trip/supplier/self/lvmama/vo/Travellers.java
@@ -1,5 +1,7 @@
 package com.huoli.trip.supplier.self.lvmama.vo;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.List;
 
@@ -10,6 +12,7 @@ import java.util.List;
  * @Description:
  * @date 2021/5/2416:30
  */
+@Data
 public class Travellers implements Serializable {
     List<Traveller> traveller;
 }
