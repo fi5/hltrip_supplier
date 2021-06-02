c5cb4665f5d8282aaab35c54230206a8de7a2da4
fengzhiqiang <fengzq@133.cn>
2021-06-02 17:41:18 +0800

    1.重发凭证

---
+++ hltrip_supplier_self/src/main/java/com/huoli/trip/supplier/self/lvmama/vo/request/LmmResendCodeOrder.java
@@ -0,0 +1,27 @@
+package com.huoli.trip.supplier.self.lvmama.vo.request;
+
+/**
+ * 描述：<br/>
+ * 版权：Copyright (c) 2011-2020<br>
+ * 公司：活力天汇<br>
+ * 作者：冯志强<br>
+ * 版本：1.0<br>
+ * 创建日期：2021/6/2<br>
+ */
+public class LmmResendCodeOrder {
+    /**
+     * 分销商订单号
+     */
+    private String partnerOrderNo;
+
+    /**
+     * 驴妈妈订单号
+     */
+    private String orderId;
+
+    public LmmResendCodeOrder(String partnerOrderNo, String orderId) {
+        this.partnerOrderNo = partnerOrderNo;
+        this.orderId = orderId;
+    }
+
+}
