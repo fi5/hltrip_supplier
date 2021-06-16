7e249f04f92cfc704c6f55d7c60819e59e5bd168
fengzhiqiang <fengzq@133.cn>
2021-06-16 14:26:46 +0800

    1.短信

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/util/SendMessageUtil.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/util/SendMessageUtil.java
@@ -22,7 +22,7 @@ import javax.annotation.Resource;
 public class SendMessageUtil {
 
 /*    @Reference(group = "${dubbo_group}", timeout = 30000, *//*check = false,*//* retries = 3)*/
-    @Reference(group = "flight-dubbo-qa")
+    @Reference(group = "flight-dubbo-online")
     private SmsService smsService;
 
     private static final String content1 = "您好，您有新的旅游产品支付订单，订单号：%S；请尽快前往后台管理系统处理，谢谢";
