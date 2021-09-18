085428d0989689251fc1deb16cf54c4fff2fe900
fengzhiqiang <fengzq@133.cn>
2021-09-18 10:31:16 +0800

    1.退款定时任务

--- hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/task/RefundNotifyTask.java
+++ hltrip_supplier_web/src/main/java/com/huoli/trip/supplier/web/task/RefundNotifyTask.java
@@ -39,37 +39,28 @@ public class RefundNotifyTask {
     @Autowired
     private UBROrderService ubrOrderService;
 
-    @Autowired
-    private ThreadPoolTaskExecutor threadPool;
-
-    @Autowired
-    private HuoliTrace huoliTrace;
-
     @Scheduled(cron="0 0 0/1 * * ?")
     public void notifyRefundDFY(){
         if(schedule == null || !StringUtils.equalsIgnoreCase("yes", schedule)){
             return;
         }
         log.info("开始执行notifyRefundDFY任务。。。");
-        threadPool.execute(() -> {
-            huoliTrace.continueSpan(huoliTrace.currentSpan());
-            long s = System.currentTimeMillis();
-            try {
-                dfyOrderService.processNotifyTicket();
-                long t = System.currentTimeMillis() - s;
-                log.info("执行笛风云门票通知退款任务完成。用时{}", DateTimeUtil.format(DateTimeUtil.toGreenWichTime(new Date(t)), "HH:mm:ss"));
-            } catch (Exception e) {
-                log.error("笛风云门票通知退款异常", e);
-            }
-            s = System.currentTimeMillis();
-            try {
-                dfyOrderService.processNotifyTour();
-                long t = System.currentTimeMillis() - s;
-                log.info("执行笛风云跟团游通知退款任务完成。用时{}", DateTimeUtil.format(DateTimeUtil.toGreenWichTime(new Date(t)), "HH:mm:ss"));
-            } catch (Exception e) {
-                log.error("笛风云跟团游通知退款异常", e);
-            }
-        });
+        long s = System.currentTimeMillis();
+        try {
+            dfyOrderService.processNotifyTicket();
+            long t = System.currentTimeMillis() - s;
+            log.info("执行笛风云门票通知退款任务完成。用时{}", DateTimeUtil.format(DateTimeUtil.toGreenWichTime(new Date(t)), "HH:mm:ss"));
+        } catch (Exception e) {
+            log.error("笛风云门票通知退款异常", e);
+        }
+        s = System.currentTimeMillis();
+        try {
+            dfyOrderService.processNotifyTour();
+            long t = System.currentTimeMillis() - s;
+            log.info("执行笛风云跟团游通知退款任务完成。用时{}", DateTimeUtil.format(DateTimeUtil.toGreenWichTime(new Date(t)), "HH:mm:ss"));
+        } catch (Exception e) {
+            log.error("笛风云跟团游通知退款异常", e);
+        }
         log.info("执行notifyRefundDFY任务完成");
     }
 
@@ -79,18 +70,14 @@ public class RefundNotifyTask {
             return;
         }
         log.info("开始执行notifyRefundBTG任务。。。");
-        threadPool.execute(() -> {
-            try {
-                huoliTrace.continueSpan(huoliTrace.currentSpan());
-                log.info("异步执行notifyRefundBTG任务。。。");
-                long s = System.currentTimeMillis();
-                ubrOrderService.processNotify();
-                long t = System.currentTimeMillis() - s;
-                log.info("执行btg通知退款任务。用时{}", DateTimeUtil.format(DateTimeUtil.toGreenWichTime(new Date(t)), "HH:mm:ss"));
-            } catch (Exception e) {
-                log.error("btg通知退款异常", e);
-            }
-        });
+        try {
+            long s = System.currentTimeMillis();
+            ubrOrderService.processNotify();
+            long t = System.currentTimeMillis() - s;
+            log.info("执行btg通知退款任务。用时{}", DateTimeUtil.format(DateTimeUtil.toGreenWichTime(new Date(t)), "HH:mm:ss"));
+        } catch (Exception e) {
+            log.error("btg通知退款异常", e);
+        }
         log.info("执行notifyRefundBTG任务完成");
     }
 }
